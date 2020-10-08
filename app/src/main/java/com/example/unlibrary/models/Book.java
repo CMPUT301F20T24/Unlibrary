f24f0a6732a2631da765123fe9807898721b1818
Armianto Sumitro <sumitro@ualberta.ca>
2020-10-08 16:03:29 -0600

    Generate getters and setters for Book

--- app/src/main/java/com/example/unlibrary/models/Book.java
+++ app/src/main/java/com/example/unlibrary/models/Book.java
@@ -1,5 +1,7 @@
 package com.example.unlibrary.models;
 
+import java.util.ArrayList;
+
 public class Book {
     enum Status {
         AVAILABLE,
@@ -12,6 +14,60 @@ public class Book {
     String description;
     String title;
     String author;
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public ArrayList<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(ArrayList<String> photos) {
+        this.photos = photos;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     ArrayList<String> photos;
     Status status;
+
+    public Book(String isbn, String title) {
+        this.isbn = isbn;
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
 }
