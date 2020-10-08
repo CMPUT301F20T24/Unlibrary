38e71b32293da2b134b9ef128b3004278bc99526
Armianto Sumitro <sumitro@ualberta.ca>
2020-10-08 16:20:00 -0600

    Fix wranings

--- app/src/main/java/com/example/unlibrary/library/LibraryBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/example/unlibrary/library/LibraryBooksRecyclerViewAdapter.java
@@ -24,10 +24,11 @@ public class LibraryBooksRecyclerViewAdapter extends RecyclerView.Adapter<BookVi
         mValues = items;
     }
 
+    @NonNull
     @Override
-    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflaytor = LayoutInflater.from(parent.getContext());
-        FragmentLibraryBookBinding libraryBookBinding = FragmentLibraryBookBinding.inflate(layoutInflaytor, parent,false);
+    public BookViewHolder<FragmentLibraryBookBinding> onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        FragmentLibraryBookBinding libraryBookBinding = FragmentLibraryBookBinding.inflate(layoutInflater, parent,false);
         return new BookViewHolder<>(libraryBookBinding);
     }
 
