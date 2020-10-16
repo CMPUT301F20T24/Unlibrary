fabb0f9edc5f017b1a6e7e4b898cc0bd717ff428
Caleb Schoepp <caleb.schoepp@gmail.com>
2020-10-15 22:49:13 -0600

    Add singleLiveEvent and upgrade to java 8

--- app/src/main/java/com/example/unlibrary/auth/AuthActivity.java
+++ app/src/main/java/com/example/unlibrary/auth/AuthActivity.java
@@ -3,10 +3,12 @@ package com.example.unlibrary.auth;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.unlibrary.R;
 
@@ -20,6 +22,8 @@ public class AuthActivity extends AppCompatActivity {
         setContentView(R.layout.activity_auth);
 
         mViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+        mViewModel.getInvalidSignupEvent().observe(this, this::showToast);
+        mViewModel.getRegisterNavigationEvent().observe(this, s -> showFragment(new RegisterFragment()));
         showFragment(new LoginFragment());
     }
 
@@ -28,4 +32,10 @@ public class AuthActivity extends AppCompatActivity {
         ft.replace(R.id.login_register_card_holder, fragment);
         ft.commit();
     }
+
+    public void showToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
 }
+
+// TODO consolidate on signup vs. register and signin vs. login
--- app/src/main/java/com/example/unlibrary/auth/AuthViewModel.java
+++ app/src/main/java/com/example/unlibrary/auth/AuthViewModel.java
@@ -3,10 +3,15 @@ package com.example.unlibrary.auth;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.unlibrary.util.SingleLiveEvent;
+
 public class AuthViewModel extends ViewModel {
 
     private MutableLiveData<String> mEmail = new MutableLiveData<>();
-    private MutableLiveData<String> mPassword = new MutableLiveData<>();
+    private MutableLiveData<String> mPassword;
+    private SingleLiveEvent<String> mInvalidSignupEvent;
+    private SingleLiveEvent<Void> mRegisterNavigationEvent;
+
 
     public MutableLiveData<String> getEmail() {
         if (mEmail == null) {
@@ -22,13 +27,36 @@ public class AuthViewModel extends ViewModel {
         return mPassword;
     }
 
+    public SingleLiveEvent<Void> getRegisterNavigationEvent() {
+        if (mRegisterNavigationEvent == null) {
+            mRegisterNavigationEvent = new SingleLiveEvent<Void>();
+        }
+        return mRegisterNavigationEvent;
+    }
+
+    public SingleLiveEvent<String> getInvalidSignupEvent() {
+        if (mInvalidSignupEvent == null) {
+            mInvalidSignupEvent = new SingleLiveEvent<String>();
+        }
+        return mInvalidSignupEvent;
+    }
+
     public void signUp() {
-        System.out.println(mEmail.getValue());
+        mRegisterNavigationEvent.call();
     }
 
     public void signIn() {
-        System.out.println("hello");
-        System.out.println(mEmail.getValue());
+        // Validate data
+        if (mEmail.getValue() == null || mEmail.getValue().isEmpty()) {
+            mInvalidSignupEvent.setValue("Missing email.");
+        } else if (mPassword.getValue() == null || mPassword.getValue().isEmpty()) {
+            mInvalidSignupEvent.setValue("Missing password.");
+        }
+
+        // Try to authenticate the user
+
+//        mInvalidSignupEvent.setValue("Invalid email or password.");
     }
+
 }
 
