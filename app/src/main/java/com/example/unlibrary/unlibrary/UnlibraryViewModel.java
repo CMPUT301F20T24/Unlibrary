250a012e00ac76e594415d946760d3a76a0ab0f2
Armianto Sumitro <sumitro@ualberta.ca>
2020-10-23 01:58:32 -0600

    Define UnlibraryViewModel

---
+++ app/src/main/java/com/example/unlibrary/unlibrary/UnlibraryViewModel.java
@@ -0,0 +1,30 @@
+package com.example.unlibrary.unlibrary;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.unlibrary.models.Book;
+
+import java.util.ArrayList;
+
+public class UnlibraryViewModel extends ViewModel {
+    private LiveData<ArrayList<Book>> mBooks;
+    private UnlibraryRepository mUnlibraryRepository;
+
+    /**
+     * Constructor for the Library ViewModel. Instantiates listener to Firestore.
+     */
+    public UnlibraryViewModel() {
+        mUnlibraryRepository = new UnlibraryRepository();
+        mBooks = mUnlibraryRepository.getBooks();
+    }
+
+    /**
+     * Getter for the mBooks object.
+     *
+     * @return LiveData<ArrayList < Book>> This returns the mBooks object
+     */
+    public LiveData<ArrayList<Book>> getBooks() {
+        return this.mBooks;
+    }
+}
