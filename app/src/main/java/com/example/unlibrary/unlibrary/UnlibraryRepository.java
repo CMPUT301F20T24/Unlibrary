b56315d870d3bb5c84fec1dcc6425b8a3153b766
Armianto Sumitro <sumitro@ualberta.ca>
2020-10-23 01:03:35 -0600

    Generate mock UnlibraryRepository

---
+++ app/src/main/java/com/example/unlibrary/unlibrary/UnlibraryRepository.java
@@ -0,0 +1,24 @@
+package com.example.unlibrary.unlibrary;
+
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.unlibrary.models.Book;
+
+import java.util.ArrayList;
+
+public class UnlibraryRepository {
+    private MutableLiveData<ArrayList<Book>> mBooks;
+
+    public UnlibraryRepository() {
+        // TODO: Unmock data
+        ArrayList<Book> books = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            books.add(new Book("abcd-1234", "Crafting the interpreter", "https://craftinginterpreters.com/", "me", null));
+        }
+        mBooks = new MutableLiveData<>(books);
+    }
+
+    public MutableLiveData<ArrayList<Book>> getBooks() {
+        return mBooks;
+    }
+}
