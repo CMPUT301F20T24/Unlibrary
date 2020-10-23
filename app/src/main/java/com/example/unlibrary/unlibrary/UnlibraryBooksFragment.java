6bc9d3fefc9e2605e8e1112ea3ed532a87fd824c
Armianto Sumitro <sumitro@ualberta.ca>
2020-10-23 02:58:14 -0600

    Simplify UnlibraryBooksFragment

--- app/src/main/java/com/example/unlibrary/unlibrary/UnlibraryBooksFragment.java
+++ app/src/main/java/com/example/unlibrary/unlibrary/UnlibraryBooksFragment.java
@@ -8,24 +8,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.unlibrary.R;
-import com.example.unlibrary.models.Book;
-
-import java.util.ArrayList;
 
 /**
- * A fragment representing a list of Items.
+ * A fragment representing a list of Books in Unlibrary page.
  */
 public class UnlibraryBooksFragment extends Fragment {
 
     private static final String ARG_COLUMN_COUNT = "column-count";
     private UnlibraryViewModel mViewModel;
-    private Observer<ArrayList<Book>> mBookListObserver;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -34,29 +29,17 @@ public class UnlibraryBooksFragment extends Fragment {
     public UnlibraryBooksFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
-    public static com.example.unlibrary.library.LibraryBooksFragment newInstance(int columnCount) {
-        com.example.unlibrary.library.LibraryBooksFragment fragment = new com.example.unlibrary.library.LibraryBooksFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_COLUMN_COUNT, columnCount);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = new ViewModelProvider(requireActivity()).get(UnlibraryViewModel.class);
     }
 
-    /**
-     * Access Library viewModel and setup data-binding and observer for changes to books.
-     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_library_book_list, container, false);
 
+        // Setup ViewModel and bind RecyclerViewAdapter
         if (view instanceof RecyclerView) {
             Context context = view.getContext();
             RecyclerView recyclerView = (RecyclerView) view;
@@ -68,7 +51,8 @@ public class UnlibraryBooksFragment extends Fragment {
             // Set the adapter
             recyclerView.setAdapter(adapter);
 
-            // observe LiveData from ViewModel
+            // Observe LiveData from ViewModel
+            // Assumes ViewModel array still points to the same object and objects are appended there
             mViewModel.getBooks().observe(getViewLifecycleOwner(), books -> adapter.notifyDataSetChanged());
         }
 
