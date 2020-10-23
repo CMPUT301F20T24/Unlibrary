d419deed3d081cbd6b1f2c5b6114e555c9487639
Armianto Sumitro <sumitro@ualberta.ca>
2020-10-23 16:04:09 -0600

    Display button on top of list to illustrate customizability

--- app/src/main/java/com/example/unlibrary/unlibrary/UnlibraryFragment.java
+++ app/src/main/java/com/example/unlibrary/unlibrary/UnlibraryFragment.java
@@ -11,11 +11,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.unlibrary.BR;
 import com.example.unlibrary.book_list.BooksFragment;
 import com.example.unlibrary.databinding.FragmentUnlibraryBinding;
 
@@ -51,6 +53,11 @@ public class UnlibraryFragment extends Fragment {
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         mBinding = FragmentUnlibraryBinding.inflate(inflater, container, false);
+
+        // TODO: Remove
+        // Dummy function to illustrate how the fragments can be customized to suit its needs
+        mBinding.setVariable(BR.filterButtonClickListener, (View.OnClickListener) view ->
+                Toast.makeText(getContext(), "Filter button clicked", Toast.LENGTH_LONG).show());
 
         // Child fragments are can only be accessed on view creation, so this is the earliest
         // point where we can specify the data source
