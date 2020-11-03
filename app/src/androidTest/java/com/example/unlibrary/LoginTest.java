3abd649b892c41bf1be867de6e5b64f31fa8fc1d
ghassanzadeh <ghass@LAPTOP-95PNFGKQ.localdomain>
2020-11-03 12:04:36 -0500

    used ActivityScenarioRule

--- app/src/androidTest/java/com/example/unlibrary/LoginTest.java
+++ app/src/androidTest/java/com/example/unlibrary/LoginTest.java
@@ -6,9 +6,11 @@ import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
+//import androidx.test.rule.ActivityTestRule;
+//import androidx.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -31,7 +33,7 @@ import static org.hamcrest.Matchers.allOf;
 public class LoginTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityScenarioRule<MainActivity> mActivityTestRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
     public void loginTest() {
@@ -73,7 +75,6 @@ public class LoginTest {
                 description.appendText("Child at position " + position + " in parent ");
                 parentMatcher.describeTo(description);
             }
-
             @Override
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
