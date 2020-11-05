98a9c439e60cc20684cbed8f5e82f93062cface3
Cyrus Diego <cyrus.d04@gmail.com>
2020-11-05 16:28:41 -0700

    ui test for bottom navigation bar

--- app/src/androidTest/java/com/example/unlibrary/BottomNavigationTest.java
+++ app/src/androidTest/java/com/example/unlibrary/BottomNavigationTest.java
@@ -4,23 +4,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import androidx.fragment.app.testing.FragmentScenario;
-import androidx.navigation.Navigation;
-import androidx.navigation.testing.TestNavHostController;
-import androidx.test.annotation.UiThreadTest;
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.core.app.ApplicationProvider;
-import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.espresso.matcher.BoundedMatcher;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import com.example.unlibrary.library.LibraryFragment;
-import com.example.unlibrary.unlibrary.UnlibraryFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -29,28 +19,28 @@ import java.util.Map;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static junit.framework.TestCase.assertEquals;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
 import static org.mockito.Mockito.mock;
 
 @RunWith(AndroidJUnit4.class)
 public class BottomNavigationTest extends MockLogin {
 
-    private static Map<Integer, String> navBarMenuItems = Map.of(
+    private static Map<Integer, String> mNavBarItems = Map.of(
             R.id.libraryFragment, "Library",
             R.id.unlibraryFragment, "Unlibrary",
             R.id.exchangeFragment, "Exchange",
             R.id.profileFragment, "Profile"
     );
 
-    private static BottomNavigationView mBottomNavigationView;
-
     // https://www.vogella.com/tutorials/AndroidTestingEspresso/article.html
     public static Matcher<View> withUnlibraryMenuItems() {
         return new BoundedMatcher<View, BottomNavigationView>(BottomNavigationView.class) {
             @Override
             public void describeTo(Description description) {
-
             }
 
             @Override
@@ -60,7 +50,7 @@ public class BottomNavigationTest extends MockLogin {
                     MenuItem current = menu.getItem(i);
                     int id = current.getItemId();
                     String title = (String) current.getTitle();
-                    if (!(navBarMenuItems.containsKey(id)) && !(navBarMenuItems.get(id).equals(title))) {
+                    if (!(mNavBarItems.containsKey(id)) && !(mNavBarItems.get(id).equals(title))) {
                         return false;
                     }
                 }
@@ -69,16 +59,35 @@ public class BottomNavigationTest extends MockLogin {
         };
     }
 
+    public static Matcher<View> isChecked(int id) {
+        return new BoundedMatcher<View, BottomNavigationView>(BottomNavigationView.class) {
+            @Override
+            public void describeTo(Description description) {
+            }
+
+            @Override
+            protected boolean matchesSafely(BottomNavigationView bottomNavigationView) {
+                return bottomNavigationView.getMenu().findItem(id).isChecked();
+            }
+        };
+    }
+
     @Test
-    public void testItems() {
+    public void menuItemsTest() {
         onView(withId(R.id.bottom_navigation)).check(matches(withUnlibraryMenuItems()));
     }
 
     @Test
-    public void testNavigationSelectionListener() {
-        MainActivity activity = activityScenarioRule.getScenario();
-        BottomNavigationView.OnNavigationItemSelectedListener mockedListener =
-                mock(BottomNavigationView.OnNavigationItemSelectedListener.class);
+    public void selectedMenuItemTest() {
+        for (Map.Entry<Integer, String> entry : mNavBarItems.entrySet()) {
+            onView(
+                    allOf(
+                            withText(entry.getValue()),
+                            isDescendantOfA(withId(R.id.bottom_navigation)),
+                            isDisplayed()
+                    )
+            ).perform(click());
+            onView(withId(R.id.bottom_navigation)).check(matches(isChecked(entry.getKey())));
+        }
     }
-
 }
