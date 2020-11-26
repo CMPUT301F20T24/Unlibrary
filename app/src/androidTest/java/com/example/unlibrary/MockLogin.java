256c4e0a5510ba3cddf15ef97497022301ba98a3
Armianto Sumitro <sumitro@ualberta.ca>
2020-11-26 13:49:45 -0700

    Halt MockLogin until user is signed in

--- app/src/androidTest/java/com/example/unlibrary/MockLogin.java
+++ app/src/androidTest/java/com/example/unlibrary/MockLogin.java
@@ -10,17 +10,25 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeUnit;
+
+import static org.awaitility.Awaitility.await;
+
 @RunWith(AndroidJUnit4.class)
 public abstract class MockLogin {
-    private final String mEmail = "cdiego@ualberta.ca";
-    private final String mPassword = "password123";
-
     @Rule
     public final ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
 
+    private final String mEmail = "cdiego@ualberta.ca";
+    private final String mPassword = "password123";
+    private boolean mIsSignedIn = false;
+
     @Before
     public void login() {
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(mEmail, mPassword);
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(mEmail, mPassword)
+                .addOnSuccessListener(authResult -> mIsSignedIn = true);
+
+        await().atMost(5, TimeUnit.SECONDS).until(() -> mIsSignedIn);
     }
 
     @After
