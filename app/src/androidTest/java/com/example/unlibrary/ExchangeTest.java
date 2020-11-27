64a16c801bcc494873d3f1ddccfbb4bdf736ffbc
Armianto Sumitro <sumitro@ualberta.ca>
2020-11-27 14:54:28 -0700

    Verify if book title is the same and toast request is made is displayed

--- app/src/androidTest/java/com/example/unlibrary/ExchangeTest.java
+++ app/src/androidTest/java/com/example/unlibrary/ExchangeTest.java
@@ -7,34 +7,42 @@ import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.filters.LargeTest;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.junit.Rule;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
 
-@LargeTest
-@RunWith(AndroidJUnit4.class)
 public class ExchangeTest extends MockLogin {
+    private static MainActivity mActivity;
+    private final int SLEEP_TIME = 5000; // milliseconds
 
-    @Rule
-    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+    @ClassRule
+    public static ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @BeforeClass
+    public static void setupActivity() {
+        // Get activity from scenario rule
+        rule.getScenario().onActivity(activity -> mActivity = activity);
+    }
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
@@ -56,7 +64,7 @@ public class ExchangeTest extends MockLogin {
     }
 
     @Test
-    public void searchBookTest() {
+    public void searchBookTest() throws InterruptedException {
         // Navigate to exchange view
         ViewInteraction exchangeBottomNav = onView(
                 allOf(withId(R.id.exchangeFragment), withContentDescription("Exchange"),
@@ -76,11 +84,16 @@ public class ExchangeTest extends MockLogin {
                                 0)));
         bookMatch.perform(actionOnItemAtPosition(0, click()));
 
-        // TODO: Verify title
+        // This is a hack because we don't use idling resources on our network calls
+        Thread.sleep(SLEEP_TIME);
+
+        // Ensure book is there
+        ViewInteraction bookTitle = onView(allOf(withId(R.id.textView6), withText("Hit Refresh")));
+        bookTitle.check(matches(isDisplayed()));
     }
 
     @Test
-    public void requestBookTest() {
+    public void requestBookTest() throws InterruptedException {
         // Go to exchange
         ViewInteraction bottomNavigationItemView = onView(
                 allOf(withId(R.id.exchangeFragment), withContentDescription("Exchange"),
@@ -96,13 +109,20 @@ public class ExchangeTest extends MockLogin {
                                 0)));
         recyclerView.perform(actionOnItemAtPosition(0, click()));
 
+        // This is a hack because we don't use idling resources on our network calls
+        Thread.sleep(SLEEP_TIME);
+
         // Click make request
         ViewInteraction requestButton = onView(
-                allOf(withId(R.id.add_request), withContentDescription("Edit book"),
+                allOf(withId(R.id.add_request), withContentDescription(R.string.add_request_description),
                         isDescendantOfA(withId(R.id.exchange_book_frame)),
                         isDisplayed()));
         requestButton.perform(click());
 
-        // TODO: Verify request made
+        // Ensure request successfully sent message is sent
+        // https://stackoverflow.com/questions/28390574/checking-toast-message-in-android-espresso
+        ViewInteraction successToast = onView(withText("Request successfully sent"))
+                .inRoot(withDecorView(not(mActivity.getWindow().getDecorView())));
+        successToast.check(matches(isDisplayed()));
     }
 }
