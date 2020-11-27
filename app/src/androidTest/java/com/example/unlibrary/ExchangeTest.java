d15091279495001ce1702db30a44859f94bdaba6
Armianto Sumitro <sumitro@ualberta.ca>
2020-11-27 14:24:40 -0700

    Make test more readable

--- app/src/androidTest/java/com/example/unlibrary/ExchangeTest.java
+++ app/src/androidTest/java/com/example/unlibrary/ExchangeTest.java
@@ -6,8 +6,8 @@ import android.view.ViewGroup;
 import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.hamcrest.Description;
@@ -18,101 +18,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class ExchangeTest extends MockLogin{
+public class ExchangeTest extends MockLogin {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
-    @Test
-    public void searchBookTest() {
-
-        ViewInteraction bottomNavigationItemView = onView(
-                allOf(withId(R.id.exchangeFragment), withContentDescription("Exchange"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.bottom_navigation),
-                                        0),
-                                2),
-                        isDisplayed()));
-        bottomNavigationItemView.perform(click());
-
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.search_exchange_book),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.nav_host_fragment),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatEditText.perform(replaceText("hit refresh"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.search_exchange_book), withText("hit refresh"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.nav_host_fragment),
-                                        0),
-                                0),
-                        isDisplayed()));
-        appCompatEditText2.perform(pressImeActionButton());
-
-        pressBack();
-
-        ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.list),
-                        childAtPosition(
-                                withId(R.id.exchange_book_list),
-                                0)));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
-
-        // TODO: Verify title
-    }
-
-    @Test
-    public void requestBookTest() {
-        ViewInteraction bottomNavigationItemView = onView(
-                allOf(withId(R.id.exchangeFragment), withContentDescription("Exchange"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.bottom_navigation),
-                                        0),
-                                2),
-                        isDisplayed()));
-        bottomNavigationItemView.perform(click());
-
-        ViewInteraction recyclerView = onView(
-                allOf(withId(R.id.list),
-                        childAtPosition(
-                                withId(R.id.exchange_book_list),
-                                0)));
-        recyclerView.perform(actionOnItemAtPosition(0, click()));
-
-        ViewInteraction floatingActionButton = onView(
-                allOf(withId(R.id.add_request), withContentDescription("Edit book"),
-                        childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.exchange_book_frame),
-                                        0),
-                                1),
-                        isDisplayed()));
-        floatingActionButton.perform(click());
-
-        // TODO: Verify request made
-    }
+    public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
 
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
@@ -131,5 +53,56 @@ public class ExchangeTest extends MockLogin{
                         && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
+    }
+
+    @Test
+    public void searchBookTest() {
+        // Navigate to exchange view
+        ViewInteraction exchangeBottomNav = onView(
+                allOf(withId(R.id.exchangeFragment), withContentDescription("Exchange"),
+                        isDescendantOfA(withId(R.id.bottom_navigation)),
+                        isDisplayed()));
+        exchangeBottomNav.perform(click());
+
+        // Search for hit refresh book
+        ViewInteraction searchBar = onView(withId(R.id.search_exchange_book));
+        searchBar.perform(replaceText("hit refresh"), pressImeActionButton(), closeSoftKeyboard());
+
+        // Click on first match
+        ViewInteraction bookMatch = onView(
+                allOf(withId(R.id.list),
+                        childAtPosition(
+                                withId(R.id.exchange_book_list),
+                                0)));
+        bookMatch.perform(actionOnItemAtPosition(0, click()));
+
+        // TODO: Verify title
+    }
+
+    @Test
+    public void requestBookTest() {
+        // Go to exchange
+        ViewInteraction bottomNavigationItemView = onView(
+                allOf(withId(R.id.exchangeFragment), withContentDescription("Exchange"),
+                        isDescendantOfA(withId(R.id.bottom_navigation)),
+                        isDisplayed()));
+        bottomNavigationItemView.perform(click());
+
+        // Click on first book
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.list),
+                        childAtPosition(
+                                withId(R.id.exchange_book_list),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(0, click()));
+
+        // Click make request
+        ViewInteraction requestButton = onView(
+                allOf(withId(R.id.add_request), withContentDescription("Edit book"),
+                        isDescendantOfA(withId(R.id.exchange_book_frame)),
+                        isDisplayed()));
+        requestButton.perform(click());
+
+        // TODO: Verify request made
     }
 }
